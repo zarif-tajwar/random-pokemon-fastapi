e8115ccb97527c7615261f15339ef129b43dd9dc
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-12-04 11:52:44 +0600

    feat: fetch random pokemon data

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import random
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
 
 @app.get("/random-pokemon")
 async def get_random_pokemon():
-    return
+    random_id = random.randint(1, 1010)
+    url = f"https://pokeapi.co/api/v2/pokemon/{random_id}"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        if response.status_code == 200:
+            pokemon_data = response.json()
+            return pokemon_data
+        else:
+            return {"error": "Failed to fetch Pokémon data"}
